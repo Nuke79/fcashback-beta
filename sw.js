ae4293b088602dfcf1b50883e0bf673b249a072d
Nuke79 <nuke1301@gmail.com>
2026-04-27 18:54:55 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cashback-beta-v13';
+const CACHE_NAME = 'cashback-beta-v14';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png'];
 
 // Install: cache all assets
